c70fff824fb335c75fc6c968d0b39cf30f503831
Jesse Connell <ancon@upenn.edu>
2017-10-12 11:25:52 -0400

    initial failing tests for summarize.sample

---
+++ tests/testthat/test_summarize_sample.R
@@ -0,0 +1,15 @@
+test_that("summarize.sample summarizes sample attributes", {
+  fail("test not yet implemented")
+})
+
+test_that("summarize.sample handles completely emtpy input sample data", {
+  fail("test not yet implemented")
+})
+
+test_that("summarize.sample handles empty sequences in input sample data", {
+  fail("test not yet implemented")
+})
+
+test_that("summarize.sample marks stutter removal", {
+  fail("test not yet implemented")
+})
