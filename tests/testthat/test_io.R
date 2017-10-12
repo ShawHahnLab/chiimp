159b9d434ece17478dae17a6012f4a6b01963e58
Jesse Connell <ancon@upenn.edu>
2017-10-12 17:17:18 -0400

    initial failing tests for io

---
+++ tests/testthat/test_io.R
@@ -0,0 +1,11 @@
+test_that("load.locus_attrs parses locus details", {
+  fail("test not yet implemented")
+})
+
+test_that("load.locus_attrs handles incorrect row/col names", {
+  fail("test not yet implemented")
+})
+
+test_that("prepare.dataset parses file paths", {
+  fail("test not yet implemented")
+})
