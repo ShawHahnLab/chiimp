aca4eff4c04308adfaabbd08664b223c6584ec57
Jesse Connell <ancon@upenn.edu>
2017-10-16 16:56:33 -0400

    summarize.sample

--- R/summarize_sample.R
+++ R/summarize_sample.R
@@ -32,16 +32,16 @@
 #' @param fraction.min numeric threshold for the minimum proportion of counts a
 #'   given entry must have, compared to the total matching all criteria for that
 #'   locus, to be considered as a potential allele.
+#' @param counts.min numeric threshold for the minimum number of counts that
+#'   must be present, in total across entries passing all filters, for potential
+#'   alleles to be considered.
 #'
 #' @return list of attributes describing the sample.
 #'
 #' @export
-summarize.sample <- function(sample.data, locus.name, fraction.min=0.05) {
+summarize.sample <- function(sample.data, locus.name, fraction.min=0.05, counts.min=500) {
   # extract sample data entries that meet all criteria for a potential allele
-  idx <- which(with(sample.data,
-                MatchingLocus == locus.name &
-                MotifMatch &
-                LengthMatch))
+  idx <- which(allele.match(sample.data, locus.name))
   chunk <- sample.data[idx, ]
   # Note that counts.locus is more restrictive than the total counts of all
   # entries with MatchingLocus equal to the given locus name, since idx includes
@@ -56,12 +56,13 @@ summarize.sample <- function(sample.data, locus.name, fraction.min=0.05) {
     chunk <- chunk[-2, ]
     stutter <- TRUE
   }
-
-  # TODO enforce count limit after all filtering
-
   # How many entries ended up above the threshold, after all filtering?  Ideally
   # this will be either one or two.
   prominent.seqs <- nrow(chunk)
+  # enforce count limit after all filtering
+  if (count.locus < counts.min) {
+    chunk <- chunk[0, ]
+  }
   # Take top to remaining entries as the two alleles and keep selected
   # attributes.
   attr.names <- c("Seq", "Count", "Length")
@@ -78,4 +79,53 @@ summarize.sample <- function(sample.data, locus.name, fraction.min=0.05) {
                            CountLocus = count.locus,
                            ProminentSeqs = prominent.seqs))
   return(sample.summary)
+}
+
+# old version of summary algorithm
+summarize.sample.by_length <- function (sample.data, locus.name,
+                                        fraction.min=0.15,
+                                        counts.min=500) {
+  idx <- which(allele.match(sample.data, locus.name))
+  chunk <- sample.data[idx, ]
+  count.total <- sum(sample.data$Count)
+  count.locus <- sum(chunk$Count)
+  chunk <- chunk %>%
+    dplyr::group_by(Length, MatchingLocus, MotifMatch, LengthMatch) %>%
+    dplyr::summarize(Count = sum(Count),
+                     Seq = first(Seq),
+                     Stutter = first(Stutter))
+  chunk <- chunk[order(chunk$Count, decreasing = T), ]
+  chunk <- chunk[chunk$Count >= fraction.min * count.locus, ]
+  stutter <- FALSE
+  if (!is.na(chunk[2, "Stutter"])) {
+    chunk <- chunk[-2, ]
+    stutter <- TRUE
+  }
+  prominent.seqs <- nrow(chunk)
+  # enforce count limit after all filtering
+  if (count.locus < counts.min)
+    chunk <- chunk[0, ]
+  # Take top to remaining entries as the two alleles and keep selected
+  # attributes.
+  attr.names <- c("Seq", "Count", "Length")
+  allele1 <- chunk[1, attr.names]
+  allele2 <- chunk[2, attr.names]
+  colnames(allele1) <- paste0("Allele1", colnames(allele1))
+  colnames(allele2) <- paste0("Allele2", colnames(allele2))
+  # Combine into summary list with additional attributes.
+  homozygous <- nrow(chunk) == 1
+  sample.summary <- c(allele1, allele2,
+                      list(Homozygous = homozygous,
+                           Stutter = stutter,
+                           CountTotal = count.total,
+                           CountLocus = count.locus,
+                           ProminentSeqs = prominent.seqs))
+  return(sample.summary)
+}
+
+allele.match <- function(sample.data, locus.name) {
+  with(sample.data,
+       as.character(MatchingLocus) == locus.name &
+         MotifMatch &
+         LengthMatch)
 }
