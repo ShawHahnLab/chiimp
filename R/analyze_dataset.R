a9e10f342c968cd36ac455b8fe50d8813130382b
Jesse Connell <ancon@upenn.edu>
2017-10-16 16:57:22 -0400

    analyze_dataset

--- R/analyze_dataset.R
+++ R/analyze_dataset.R
@@ -5,29 +5,36 @@
 
 analyze.dataset <- function(dataset,
                             locus_attrs,
-                            num.cores = max(1,
-                                as.integer(parallel::detectCores() / 2) - 1)) {
-  # Set up the cluster and export requried names.
-  cluster <- parallel::makeCluster(num.cores)
-  cluster_names <- c("locus_attrs",
-         "load.seqs",
-         "read.fastq",
-         "read.fa",
-         "summarize.sample",
-         "analyze.sample",
-         "find.matching.primer",
-         "check.motif",
-         "find.stutter",
-         "check.length")
-  parallel::clusterExport(cluster, cluster_names)
-  # Load, analyze, and summarize each sample across the cluster.
-  raw.results <- parallel::parApply(cluster, dataset, 1, function(entry) {
+                            num.cores=max(1,
+                                as.integer(parallel::detectCores() / 2) - 1),
+                            summary.function=summarize.sample) {
+  analyze.entry <- function(entry) {
     seqs <- load.seqs(entry["Filename"])
     sample.data <- analyze.sample(seqs, locus_attrs, 3)
-    sample.summary <- summarize.sample(sample.data, entry["Locus"])
+    sample.summary <- summary.function(sample.data, entry["Locus"])
     return(list(summary = sample.summary, data = sample.data))
-  })
-  parallel::stopCluster(cluster)
+  }
+  if (num.cores > 1) {
+    # Set up the cluster and export requried names.
+    cluster <- parallel::makeCluster(num.cores)
+    cluster_names <- c("locus_attrs",
+                       "load.seqs",
+                       "summarize.sample",
+                       "summarize.sample.by_length",
+                       "analyze.sample",
+                       "find.matching.primer",
+                       "check.motif",
+                       "find.stutter",
+                       "check.length")
+    # https://stackoverflow.com/a/12232695/6073858
+    parallel::clusterEvalQ(cluster, library(dnar))
+    parallel::clusterExport(cluster, cluster_names)
+    # Load, analyze, and summarize each sample across the cluster.
+    raw.results <- parallel::parApply(cluster, dataset, 1, analyze.entry)
+    parallel::stopCluster(cluster)
+  } else {
+    raw.results <- apply(dataset, 1, analyze.entry)
+  }
   # Recombined results into a summary data frame and a list of full sample data.
   tidy.analyzed.dataset(dataset, raw.results)
 }
@@ -38,5 +45,6 @@ tidy.analyzed.dataset <- function(dataset, raw.results) {
   summary <- do.call(rbind.data.frame, summaries)
   rownames(summary) <- rownames(dataset)
   names(data)       <- rownames(dataset)
+  summary <- cbind(dataset, summary)
   return(list(summary = summary, data = data))
 }
