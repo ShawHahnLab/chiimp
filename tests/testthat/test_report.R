f40123d6c28f28756fb5148981bc6cc3e88eade2
Jesse Connell <ancon@upenn.edu>
2017-12-19 12:58:21 -0500

    fix plot_alignment test
    Go figure, I can't assume I'll get the same PNG byte-for-byte on a
    totally different system.  Maybe setting some more png() options
    explicitly will still allow this idea to work.

--- tests/testthat/test_report.R
+++ tests/testthat/test_report.R
@@ -5,12 +5,12 @@ test_that("plot_alignment plots alignments for a locus", {
   with(results_summary_data, {
     alignments <- align_alleles(results$summary)
     fp_png <- tempfile()
-    png(fp_png)
+    png(fp_png, width = 480, height = 480, antialias = "none")
     plot_alignment(alignments[["A"]])
     dev.off()
     data_png <- readBin(fp_png, n=1e6, raw())
     hash <- openssl::md5(data_png)
-    known <- unlist(strsplit("62:57:60:b9:da:13:a8:24:b2:0a:ce:ab:49:1b:62:0a",
+    known <- unlist(strsplit("88:89:3e:cc:32:69:0b:9a:34:26:45:fe:36:c1:dc:9c",
                            ":"))
     expect_true(all(hash == known))
   })
