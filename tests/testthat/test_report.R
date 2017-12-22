803cd715ad4bef3b1a8122f66651b11303e8de47
Jesse Connell <ancon@upenn.edu>
2017-12-22 16:00:34 -0500

    debug report test

--- tests/testthat/test_report.R
+++ tests/testthat/test_report.R
@@ -4,36 +4,15 @@
 test_that("plot_alignment plots alignments for a locus", {
   with(results_summary_data, {
     alignments <- align_alleles(results$summary)
-    fp_png <- tempfile()
-    bmp(fp_png,
-        width = 100, height = 100, units = "px", pointsize = 2, bg="white",
-        res = NA, type = "cairo", antialias = "none")
+    fp_img <- tempfile()
+    svg(fp_img)
     plot_alignment(alignments[["A"]])
     dev.off()
-    data_png <- readBin(fp_png, n=1e6, raw())
-    hash <- openssl::md5(data_png)
-    #known_txt <- "22:5b:4a:e9:dd:c7:65:fe:78:30:37:ec:83:d5:b5:ac" # png travis
-    #known_txt <- "88:89:3e:cc:32:69:0b:9a:34:26:45:fe:36:c1:dc:9c" # png
-    #known_txt <- "20:d6:97:61:f8:fd:26:dc:f4:d3:35:16:03:4b:59:b2" # bmp travis
-    known_txt <- "26:6e:41:90:5d:51:00:4c:f9:9c:2d:b9:c2:cd:64:e0" # bmp
+    data_img <- readBin(fp_img, n=1e6, raw())
+    hash <- openssl::md5(data_img)
+    known_txt <- "25:95:62:2f:b7:ee:d6:5d:41:bf:17:c1:24:16:d5:e5"
     known <- unlist(strsplit(known_txt, ":"))
-
     cat(paste0("\n\n\n", hash, "\n\n\n"), file = "/dev/stderr")
-
-    # graphical parameters
-    parlog <- file('par.log', 'w')
-    parlist <- par()
-    cat(paste(names(parlist), parlist, sep = "="), sep = "\n", file = parlog)
-    close(parlog)
-
-
-    # image data
-    binarylog <- file('binary.log', 'w')
-    for (i in seq(1, length(data_png), 16)) {
-      cat(data_png[i:min(length(data_png),i+15)], "\n", file = binarylog)
-    }
-    close(binarylog)
-
     expect_true(all(hash == known))
   })
 })
