7d69f796bb6cb5d2c0fc2b57f961118751948142
Jesse Connell <ancon@upenn.edu>
2018-02-07 11:42:36 -0500

    use CSV format for all file read/writes

--- inst/bin/demo.sh
+++ inst/bin/demo.sh
@@ -6,7 +6,7 @@
 dir=$(readlink -f $(dirname $BASH_SOURCE))
 scratch=$(mktemp -d)
 R --vanilla -q -e "devtools::load_all('.', quiet=T); test_data\$write_seqs(test_data\$seqs, '$scratch/str-dataset', 'Replicate1-Sample%s-%s.fasta')"
-cp "$dir/../example_locus_attrs.tsv" "$scratch/locus_attrs.tsv"
+cp "$dir/../example_locus_attrs.csv" "$scratch/locus_attrs.csv"
 cp "$dir/../example_config.yml" "$scratch/config.yml"
 cd "$scratch"
 "$dir/microsat" "config.yml"
