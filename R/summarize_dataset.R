d8a8abb4bbd257f007ba6efd86962eb0e8060d13
Jesse Connell <ancon@upenn.edu>
2018-05-17 16:27:14 -0400

    re-enable tally_cts_per_locus's Locus level filter

--- R/summarize_dataset.R
+++ R/summarize_dataset.R
@@ -410,6 +410,8 @@ tally_cts_per_locus <- function(results) {
     # Match sample to original, unfiltered data from sequence file
     fp <- results$summary[id, "Filename"]
     seqs <- results$files[[fp]]
+    # Just keep loci actually analyzed in this set
+    levels(seqs$MatchingLocus) <- levels(results$summary$Locus)
     # Sum counts for each locus
     sapply(split(seqs$Count, seqs$MatchingLocus), sum)
   }))
