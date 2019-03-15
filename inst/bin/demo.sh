3e3f5148b78f9eb8a588d3fda8b448a59cfaf720
Jesse Connell <ancon@upenn.edu>
2019-03-15 12:22:23 -0400

    Switch readlink -f to pwd -P in demos, for mac os
    The pwd behavior looks to be the same between Linux and Mac OS (unlike
    for readlink) so this should work on both.

--- inst/bin/demo.sh
+++ inst/bin/demo.sh
@@ -3,10 +3,12 @@
 # Wrapper script to set up and execute a demo, using the "chiimp" executable
 # R script and the test data.
 
-dir=$(readlink -f $(dirname $BASH_SOURCE))
+cd "$(dirname $BASH_SOURCE)"
+dir=$(pwd -P)
 scratch=$(mktemp -d)
-R --vanilla -q -e "devtools::load_all('.', quiet=T); test_data\$write_seqs(test_data\$seqs, '$scratch/str-dataset', 'Replicate1-Sample%s-%s.fasta')"
+R --vanilla -q -e "devtools::load_all('..', quiet=T); test_data\$write_seqs(test_data\$seqs, '$scratch/str-dataset', 'Replicate1-Sample%s-%s.fasta')" > /dev/null
 cp "$dir/../example_locus_attrs.csv" "$scratch/locus_attrs.csv"
 cp "$dir/../example_config.yml" "$scratch/config.yml"
 cd "$scratch"
 "$dir/chiimp" "config.yml"
+echo "Demo files and output stored in $(pwd -P)"
