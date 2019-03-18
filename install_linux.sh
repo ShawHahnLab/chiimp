576f0372173dd0d9a8cb4046da0b433da151461d
Jesse Connell <ancon@upenn.edu>
2019-03-18 17:11:11 -0400

    Point linux and mac installers to install.R script

--- install_linux.sh
+++ install_linux.sh
@@ -5,57 +5,7 @@
 # A base R install is assumed to already be present, but all dependencies
 # should be installed automatically here.
 
-rexe=$(which R)
+rscript=$(which Rscript)
 pkgdir=$(readlink -f $(dirname $BASH_SOURCE))
-pkgdir_r=$pkgdir
-
-devtools_setup="install.packages('devtools',repos='https://cloud.r-project.org')"
-bioclite_setup="source('https://bioconductor.org/biocLite.R');biocLite();biocLite('msa')"
-deps_setup="devtools::install_deps('$pkgdir_r',dependencies=TRUE)"
-chiimp_test="quit(save='no',status=sum(as.data.frame(devtools::test('$pkgdir_r'))\$failed))"
-chiimp_setup="devtools::install('$pkgdir_r')"
-chiimp_get_path="cat(system.file('bin','chiimp.sh',package='chiimp'))"
-
-"$rexe" --version
-echo
-echo "### Installing devtools"
-echo
-"$rexe" --slave -e "$devtools_setup"
-echo
-echo "### Installing Bioconductor and MSA"
-echo
-"$rexe" --slave -e "$bioclite_setup"
-echo
-echo "### Installing dependencies"
-echo
-"$rexe" --slave -e "$deps_setup"
-echo
-echo "### Testing CHIIMP"
-echo
-if "$rexe" --slave -e "$chiimp_test"
-then
-	echo
-	echo
-	echo  "   Warning: Tests indicated failures."
-	echo
-	echo
-fi
-echo
-echo "### Installing CHIIMP"
-echo
-"$rexe" --slave -e "$chiimp_setup"
-
-chiimp_path=$("$rexe" --slave -e "$chiimp_get_path")
-desktop_file="[Desktop Entry]
-Type=Application
-Terminal=true
-Name=CHIIMP
-Exec=$chiimp_path"
-
-if [ -d "$HOME/Desktop" ]; then
-	echo
-	echo "### Creating Desktop Icon"
-	echo
-	echo "$desktop_file" > "$HOME/Desktop/CHIIMP.desktop"
-	chmod +x "$HOME/Desktop/CHIIMP.desktop"
-fi
+"$rscript" --vanilla "$pkgdir/inst/installer/install.R"
+read -n 1 -s -p "Press any key to continue . . ."; echo # see "pause" in cmd.exe
