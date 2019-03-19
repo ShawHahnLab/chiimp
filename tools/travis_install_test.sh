18dd7c0517533cd08b28630f9320080d11ad5fd3
Jesse Connell <ancon@upenn.edu>
2019-03-19 11:06:54 -0400

    more stringent Travis installer tests

--- tools/travis_install_test.sh
+++ tools/travis_install_test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 # This is a helper script for Travis testing.
 # Assumes working directory is the project directory.
@@ -6,8 +7,13 @@
 # (These scripts pause for input hence the pipe from the yes command.)
 if   [[ $TRAVIS_OS_NAME -eq "linux"   ]]; then
 	yes | ./install_linux.sh
+	# Desktop icon should be a regular file
+	test -f ~/Desktop/CHIIMP.desktop
 elif [[ $TRAVIS_OS_NAME -eq "osx"     ]]; then
 	yes | ./install_mac.command
+	# Desktop icon should be a symbolic link to an existent file
+	test -f ~/Desktop/CHIIMP
+	test -h ~/Desktop/CHIIMP
 elif [[ $TRAVIS_OS_NAME -eq "windows" ]]; then
 	# (If and when enabled in the Travis config.)
 	yes | ./install_windows.cmd
