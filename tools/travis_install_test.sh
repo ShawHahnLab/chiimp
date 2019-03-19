b971b710471735c992c12645d7a07f5fb3aafa4e
Jesse Connell <ancon@upenn.edu>
2019-03-19 11:40:35 -0400

    Fix bash syntax error in Travis installer tests
    == for strings, -eq for numbers!  This is why it was trying to use the
    linux installer for osx.

--- tools/travis_install_test.sh
+++ tools/travis_install_test.sh
@@ -9,17 +9,17 @@ set -e
 set -x
 
 # (These scripts pause for input hence the pipe from the yes command.)
-if   [[ $TRAVIS_OS_NAME -eq "linux"   ]]; then
+if   [[ $TRAVIS_OS_NAME == "linux"   ]]; then
 	mkdir -p ~/Desktop
 	yes | ./install_linux.sh
 	# Desktop icon should be a regular file
 	test -f ~/Desktop/CHIIMP.desktop
-elif [[ $TRAVIS_OS_NAME -eq "osx"     ]]; then
+elif [[ $TRAVIS_OS_NAME == "osx"     ]]; then
 	yes | ./install_mac.command
 	# Desktop icon should be a symbolic link to an existent file
 	test -f ~/Desktop/CHIIMP
 	test -h ~/Desktop/CHIIMP
-elif [[ $TRAVIS_OS_NAME -eq "windows" ]]; then
+elif [[ $TRAVIS_OS_NAME == "windows" ]]; then
 	# (If and when enabled in the Travis config.)
 	yes | ./install_windows.cmd
 else
