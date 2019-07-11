82f8d13438bb82ce03e442c3b3ff2bdb909a0f56
Jesse Connell <ancon@upenn.edu>
2019-07-11 13:50:34 -0400

    For #41: Failing tests for stutter/artifact opts

--- tests/testthat/test_analyze_seqs.R
+++ tests/testthat/test_analyze_seqs.R
@@ -79,8 +79,27 @@ with(test_data, {
     chunk <- subset(seq_data, !is.na(Stutter))
     expect_equal(chunk$Count, c(279, 114, 2, 2, 2))
     expect_equal(chunk$Stutter, c(1, 2, 2, 2, 2))
-    # TODO add expectation for high-count case that should not be marked as
-    # stutter
+  })
+
+  test_that("analyze_seqs does not mark high-count entries as stutter", {
+    # replace a few high-count entries with one that was considered stutter
+    # in the above test.  Now none of those top sequences should be considered
+    # stutter.
+    s <- seqs1$A
+    s[nchar(s) %in% c(158, 54)] <- s[nchar(s) == 190][1]
+    seq_data <- analyze_seqs(s, locus_attrs, 3)
+    chunk <- subset(seq_data, !is.na(Stutter))
+    expect_equal(chunk$Count, c(2, 2, 2))
+    expect_equal(chunk$Stutter, c(2, 2, 2))
+  })
+
+  test_that("analyze_seqs works with varied threshold for stutter counts", {
+    s <- seqs1$A
+    s[nchar(s) %in% c(158, 54)] <- s[nchar(s) == 190][1]
+    seq_data <- analyze_seqs(s, locus_attrs, 3, stutter.count.ratio_max = 1/2)
+    chunk <- subset(seq_data, !is.na(Stutter))
+    expect_equal(chunk$Count, c(443, 2, 2, 2, 1, 1))
+    expect_equal(chunk$Stutter, c(2, 2, 2, 2, 4, 4))
   })
 
   test_that("analyze_seqs marks artifact entries", {
